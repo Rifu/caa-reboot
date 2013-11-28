96d67ca90ecb6e4f54653d3fe95d0bfdf032a279
Rifu <kuya.vinchi@gmail.com>
2013-11-27 19:23:57 -0800

    Added fix to activeadmin not precompiling css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 CaaReboot::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  # ActiveAdmin precompile hack
+  config.assets.precompile += %w[active_admin.css active_admin.js]
   # Code is not reloaded between requests
   config.cache_classes = true
 
