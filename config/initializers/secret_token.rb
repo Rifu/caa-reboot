7f72647c983438a221b1ad00f3a2addcb6877aaa
Rifu <kuya.vinchi@gmail.com>
2013-11-27 18:59:54 -0800

    Discarded security for convenience

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaaReboot::Application.config.secret_token = '8e265cdc4c8255188b42fc2f11884f0f6de9dca543b6be8aae98e1c40def4d5626a9e0cc69996acbd6930b408b81052635abcfaf5210d8d91e3c7ebdd68f2983'
