6dcc69405b01015cc2288d43c433bf01940a22df
Rifu <kuya.vinchi@gmail.com>
2013-12-07 15:23:28 -0800

    Added logic for showing archive

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,5 +1,6 @@
 class SchedulesController < ApplicationController
   def show
     @schedule = Schedule.find(params[:id])
+    @schedule_archive = Schedule.where("NOT id = ?", @schedule.id).order("created_at DESC")
   end
 end
